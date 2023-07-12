d5eea05906f93129b57efbfb3ffceaa713faac9e
santmailo <iamsunil006@gmail.com>
2023-07-12 10:16:38 +0530

    solved

--- src/components/App.js
+++ src/components/App.js
@@ -3,9 +3,17 @@ import React from "react";
 import './../styles/App.css';
 
 const App = () => {
+  const [task, setTask] = React.useState(""); 
+
+  const handleChange = (event) => {
+    setTask(event.target.value);
+  }
+
   return (
     <div>
         {/* Do not remove the main div */}
+        <input type="text" onChange={handleChange} />
+        <p>{task}</p>
     </div>
   )
 }
